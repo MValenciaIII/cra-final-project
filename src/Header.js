87733d25d93ac0a64b9a50c64a85350fdef4ddab
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2020-04-14 15:55:19 -0500

    Final Project Initalize
    This is the first commit of my Final React project.

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <h1>Valen's Form</h1>
+                        </div>
+                        <div className="col-sm-8 nav">
+                            <ul>
+                                <li>Lorem</li>
+                                <li>Lorem</li>
+                                <li>Lorem</li>
+                                <li>Lorem</li>
+                                <li>Lorem</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+
+}
+
+
+export default Header
---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <>
+            <main>
+                <div className="heroBackground">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-pull-1 col-sm-10 col-sm-push-1">
+                                <img className="img-responsive" src="https://via.placeholder.com/1200x600?text=Visit+ValensForm"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+
+}
+
+
+export default Main
